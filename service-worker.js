dbea8ba6191e4f29633c537754d3452ffb1fc6d8
riccardospigarolo-tech <riccardo.spigarolo@gmail.com>
2025-11-14 10:08:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,21 @@
-self.addEventListener('install', e => {
+const CACHE_NAME = "chrono-cache-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./chrono.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('chrono-cache').then(cache => {
-      return cache.addAll([
-        'index.html',
-        'chrono.js',
-        'manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
-
 });
